8c039f957248a10d796840339a4746524a8033ab
nerd-caveman <inioluwwa4.is@gmail.com>
2022-04-10 00:27:18 +0100

    Add documentation and more code clean up

--- frontend/src/routes/Home/settings.tsx
+++ frontend/src/routes/Home/settings.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Text, View, Alert } from "react-native";
 import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
 import { ListItem } from "react-native-elements";
--- frontend/utils/index.js
+++ frontend/utils/index.js
@@ -1,4 +1,5 @@
 import Toast from "react-native-toast-message";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const showToast = (type, title, body, position = "top") => {
   Toast.show({
@@ -8,4 +9,14 @@ export const showToast = (type, title, body, position = "top") => {
     visibilityTime: 3500,
     position,
   });
+};
+
+export const removeCredentials = async () => {
+  try {
+    await AsyncStorage.removeItem("@Auth:token");
+    // await AsyncStorage.removeItem("@Auth:expiry");
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
 };
